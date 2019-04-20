4a78e86386bbc5e5bf6cbd9a952ecdf87b0959c8
Christiansantoss <nismoskyi@yahoo.com>
2019-04-19 19:27:31 -0700

    routes

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,6 +1,6 @@
 class SayController < ApplicationController
   def hello
-    @time = Time.know #Changing our action method in the controller to set the time value into an instance variable called time
+    @time = Time.now #Changing our action method in the controller to set the time value into an instance variable called time
   end
 
   def goodbye
