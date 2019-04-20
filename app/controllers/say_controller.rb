d2a6841d8cf3e27081387f1648c30908f0b903a9
Christiansantoss <nismoskyi@yahoo.com>
2019-04-19 20:35:53 -0700

    iterating over files

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,6 +1,7 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now #Changing our action method in the controller to set the time value into an instance variable called time
+    @files = Dir.glob('*')
   end
 
   def goodbye
